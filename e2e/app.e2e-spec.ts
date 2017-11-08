c8c51a42ace5b6a55a11e9fa023046fddca60496
Sriram Madduri <srrm.madduri@gmail.com>
2017-11-09 01:37:13 +0530

    Angular4RVuncommentline3

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -11,12 +11,12 @@ describe('hello-world App', () => {
     page.navigateTo();
     expect(page.getParagraphText()).toEqual('Welcome to apaap!');
   });
-    /*
+    
    it('should display welcome message', () => {
     page.navigateTo();
     expect(page.getParagraphText()).toEqual('Welcome to venkat!');
   });
-  */
+  
    it('should display welcome message', () => {
     page.navigateTo();
     expect(page.getParagraphText()).toEqual('Welcome to app!');
